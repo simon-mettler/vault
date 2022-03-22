c8369d28a4b4e7056967bf2bfa314429d5636efd
Simon Mettler <contact@simonmettler.com>
2022-03-22 15:33:43 +0100

    basic daily log setup

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ app.set('view engine', 'pug');
 
 app.use(express.urlencoded({extended: false}));
 
-app.get('/test', (req, res) => {
+app.get('/dailylog', (req, res) => {
 	db.query('SELECT * FROM dailylogs', [], (err, res) => {
 		if (err) { 
 			throw err 
@@ -23,8 +23,17 @@ app.get('/test', (req, res) => {
 	})
 })
 
-app.post('/posttest', (req, res) => {
-	console.log(req.body)
+app.post('/dailylog', (req, res) => {
+	const {date, mood, happy, stress, paranoia, depression, fear, libido, inlove, sex_count, sex_qual, masturbation, cuddle} = req.body;
+	db.query(
+		`INSERT INTO dailylogs
+			(date, mood, happy, stress, paranoia, depression, fear, libido, inlove, sex_count, sex_qual, masturbation, cuddle)
+		VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12, $13)
+	`, [date, mood, happy, stress, paranoia, depression, fear, libido, inlove, sex_count, sex_qual, masturbation, cuddle], 
+		(err, res) => {
+		if (err) {throw err}
+	}
+	);
 	res.send('ok')
 })
 
